2a1aa965c0487519a4ae5e109b0084a5b89956fb
TsungWei Hu <marr.tw@gmail.com>
2019-09-28 23:09:00 +0800

    Update Multiplication Table

--- sample/99.py
+++ sample/99.py
@@ -4,5 +4,6 @@ y = range(1, 10)
 for i in x:
     line = ''
     for j in y:
-        line += '%s x %s = %2s  ' % (i, j, i*j)
+        # line += '%s x %s = %2s  ' % (i, j, i*j) # old fashioned
+        line += '{} x {} = {:2d} '.format(i, j, i*j)
     print(line)
